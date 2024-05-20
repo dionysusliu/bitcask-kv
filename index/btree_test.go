908e9c8a1aaffb5188f3543ec4568f69b1660a0f
chuang <chuangliu0815@gmail.com>
2024-05-20 16:42:02 +0800

    Fix bug: interface method can only be access via interface object, not interface pointer

--- index/btree_test.go
+++ index/btree_test.go
@@ -7,13 +7,13 @@ import (
 func TestBTreePutNilKey(t *testing.T) {
 	var bt Indexer = NewBTreeIndexer()
 	t.Run("TestBTree_PutNilKey", func(t *testing.T) {
-		testIndexerPutNilKey(t, &bt)
+		testIndexerPutNilKey(t, bt)
 	})
 }
 
 func TestBTreeGetNilKey(t *testing.T) {
 	var bt Indexer = NewBTreeIndexer()
 	t.Run("TestBTree_GetNilKey", func(t *testing.T) {
-		testIndexerGetNilKey(t, &bt)
+		testIndexerGetNilKey(t, bt)
 	})
 }
--- index/index_test.go
+++ index/index_test.go
@@ -6,28 +6,25 @@ import (
 	"testing"
 )
 
-func testIndexerPutNilKey(t *testing.T, indexer *Indexer) {
+func testIndexerPutNilKey(t *testing.T, indexer Indexer) {
 	assertion := assert.New(t)
-	bt := NewBTreeIndexer()
 
 	// edge case: put nil key and retrieve
-	res := bt.Put(nil, &data.LogRecordPos{
+	res := indexer.Put(nil, &data.LogRecordPos{
 		Fid:    0,
 		Offset: 100,
 	})
 	assertion.Equal(res, true, "Indexer should support nil key")
 }
 
-func testIndexerGetNilKey(t *testing.T, indexer *Indexer) {
+func testIndexerGetNilKey(t *testing.T, indexer Indexer) {
 	assertion := assert.New(t)
-	bt := NewBTreeIndexer()
-
 	// edge case: put nil key and retrieve
-	bt.Put(nil, &data.LogRecordPos{
+	indexer.Put(nil, &data.LogRecordPos{
 		Fid:    0,
 		Offset: 100,
 	})
-	resFromNilKey := bt.Get(nil)
+	resFromNilKey := indexer.Get(nil)
 	assertion.Equal(resFromNilKey.Fid, uint32(0), "Retrieval Result doesn't match")
 	assertion.Equal(resFromNilKey.Offset, int64(100), "Retrieval Result doesn't match")
 }

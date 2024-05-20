ced3e27965127cfce5580f9390e77784680eb760
chuang <chuangliu0815@gmail.com>
2024-05-20 09:19:45 +0800

    comments on Indexer interfaces

--- index/btree.go
+++ index/btree.go
@@ -14,12 +14,20 @@ type BTree struct {
 	tree *btree.BTree
 }
 
-// Interface for google-btree's items
+// NewBTreeIndexer initialize a BTree indexer
+func NewBTreeIndexer() *BTree {
+	return &BTree{
+		lock: new(sync.RWMutex),
+		tree: btree.New(32),
+	}
+}
+
+// Less Interface for google-btree's items
 func (ai Item) Less(bi btree.Item) bool {
 	return bytes.Compare(ai.key, bi.(*Item).key) == -1
 }
 
-// add new key-value pair in btree
+// Put add new key-value pair in btree
 // return true on success, otherwise return false
 // this method is thread-safe
 func (bt *BTree) Put(key []byte, pos *data.LogRecordPos) bool {
@@ -30,30 +38,25 @@ func (bt *BTree) Put(key []byte, pos *data.LogRecordPos) bool {
 	return true
 }
 
-// try to get the value from provided key
+// Get try to get the value from provided key
 // if K-V pair exists, return the value, otherwise return nil
 // this method is thread-safe
 func (bt *BTree) Get(key []byte) *data.LogRecordPos {
-	keySearch := Item{key: key}
-	bt.lock.Lock()
-	item := bt.tree.Get(keySearch)
-	bt.lock.Unlock()
+	keyItem := Item{key: key}
+	item := bt.tree.Get(keyItem) // no need to lock, go-btree is thread-safe under read
 	if item == nil {
 		return nil
 	}
 	return item.(*Item).pos
 }
 
-// try to delete a key in storage
+// Del try to delete a key in storage
 // if K-V pair exists and deleted successfully, return true, otherwise return false
 // this method is thread safe
 func (bt *BTree) Del(key []byte) bool {
-	keySearch := Item{key: key}
+	keyItem := Item{key: key}
 	bt.lock.Lock()
-	item := bt.tree.Delete(keySearch)
+	bt.tree.Delete(keyItem)
 	bt.lock.Unlock()
-	if item == nil {
-		return false
-	}
 	return true
 }
--- index/index.go
+++ index/index.go
@@ -6,9 +6,18 @@ import (
 
 // Indexer is interface definition for in-memory index
 // it stores the pair <key, log-position-in-file>
+// all methods should be thread-safe under read / write by default, except for explicit notice
 type Indexer interface {
+	// Put add K-V pair in indexer
+	// return true on success, otherwise return false
 	Put(key []byte, pos *data.LogRecordPos) bool
+
+	// Get get value with provided key
+	// return value if found, otherwise return nil
 	Get(key []byte) *data.LogRecordPos
+
+	// Del delete an K-V pair in indexer
+	// return true if K-V doesn't exist or being successfully deleted, return false otherwise
 	Del(key []byte) bool
 }
 
